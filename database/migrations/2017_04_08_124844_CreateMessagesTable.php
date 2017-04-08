83a82786b957b2c2d892cf5e84ecc611879c19e1
Lei Nelissen <lei@codified.nl>
2017-04-08 15:17:45 +0200

    Create message migration

--- database/migrations/2017_04_08_124844_CreateMessagesTable.php
+++ database/migrations/2017_04_08_124844_CreateMessagesTable.php
@@ -13,7 +13,13 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('message');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timeStamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('messages');
     }
 }
