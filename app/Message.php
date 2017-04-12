2b9b0c1d110540a4a4624d7aa9e80fd03c0c58c0
Lei Nelissen <lei@codified.nl>
2017-04-12 22:38:51 +0200

    Fix typo in message model fillables

--- app/Message.php
+++ app/Message.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        "properties"
+        "message"
     ];
 
     /**
