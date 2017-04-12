12c4af458f37fc25839d5f9e4520a7fe0963636d
Lei Nelissen <lei@codified.nl>
2017-04-12 22:39:56 +0200

    Add newMessage broadcasting event and broadcast route

---
+++ app/Events/MessageCreated.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use App\Message;
+use App\User;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The supplied new message
+     *
+     * @var Message
+     */
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = collect($message);
+        $this->message = $this->message->merge([
+            "user" => $message->user
+        ]);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PresenceChannel('chat');
+    }
+}
