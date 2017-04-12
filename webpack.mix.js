37ae031abb5b7d094b05e4ee64e5c645bbd6c096
Lei Nelissen <lei@codified.nl>
2017-04-12 21:53:50 +0200

    Setup Socket.IO-server

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require("laravel-mix");
  */
 
 mix.js("resources/assets/js/app.js", "public/js")
-    .extract(["vue", "axios"])
+    .extract(["vue", "axios", "laravel-echo", "socket.io-client"])
     .sass("resources/assets/sass/app.scss", "public/css")
     .copyDirectory("resources/assets/images", "public/images")
     .version();
