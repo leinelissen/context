c39fa9e1133ecb67fa523947e2dc904885aa184e
Lei Nelissen <lei@codified.nl>
2017-04-21 14:47:12 +0200

    Add middleware to check if user should have access to a room

---
+++ app/Http/Middleware/RoomIsOwnedByUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Room;
+use Illuminate\Support\Facades\Auth;
+
+class RoomIsOwnedByUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $parameters = $request->route()->parameters();
+
+        if (!in_array('id', $parameters)) {
+            // return redirect('/');
+        };
+
+        $room = Room::findOrFail($parameters['id']);
+        $users = $room->users;
+
+        if (!$users->contains('id', Auth::id())) {
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
