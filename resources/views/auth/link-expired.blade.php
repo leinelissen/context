c4eb9c624d1ee97d6eea568e1bdec4acec51c011
Lei Nelissen <lei@codified.nl>
2017-05-01 13:32:15 +0200

    Remove headings for dialogs where it is not necessary

--- resources/views/auth/link-expired.blade.php
+++ resources/views/auth/link-expired.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="container padding small">
-        <h1>Login</h1>
         <p>This link has expired. You can try creating a new link by <a href="{{ URL::to('/login') }}">logging in</a> again.</p>
     </div>
 @endsection
--- resources/views/auth/token.blade.php
+++ resources/views/auth/token.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="container padding small">
-        <h1>Login</h1>
         <p>An email has been sent to <b>{{ $loginRequest->user->email }}</b>. It contains a link which you can use to login.</p>
         <p>If you have not received a link, try <a href="{{ URL::to('/login') }}">logging in</a> again.</p>
     </div>
