e0debb4625b775a824b74e7a9852a9defb3579ce
Lei Nelissen <lei@codified.nl>
2017-05-07 13:49:17 +0200

    Remove redundant dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Faker\Factory as FakerFactory;
-use Faker\Generator as FakerGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
