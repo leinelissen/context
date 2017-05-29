fa589292466aac1724a445793e06d2ca0c61d04d
Lei Nelissen <lei@codified.nl>
2017-05-29 21:58:29 +0200

    Skew role distribution on seed more towards students

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Channel;
+use App\Role;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
+    public $chanceOfTeacher;
+
+    public function __construct()
+    {
+        $this->chanceOfTeacher = 15;
+    }
+
     /**
      * Run the database seeds.
      *
@@ -11,9 +21,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 30)->create()->each(function ($user) {
-            $user->channels()->attach(App\Channel::all());
-            $user->roles()->attach(App\Role::all()->random());
+        factory(User::class, 30)->create()->each(function ($user) {
+            $user->channels()->attach(Channel::all());
+
+            if (rand(0, 100) < $this->chanceOfTeacher) {
+                $user->roles()->attach(Role::where('name', 'Teacher')->get());
+            } else {
+                $user->roles()->attach(Role::where('name', 'Student')->get());
+            }
         });
     }
 }
